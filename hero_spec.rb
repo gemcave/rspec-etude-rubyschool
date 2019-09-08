baeecb6c2d2ea37ba1fc98a962ab43608bca9079
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-08 19:54:14 +0600

    add hero_info test

--- hero_spec.rb
+++ hero_spec.rb
@@ -5,14 +5,22 @@ describe Hero do
 
 		expect(hero.name).to eq 'Yan' # 'hero.name == 'Yan'
 	end
+
 	it "can power up" do
 		hero = Hero.new 'yan'
 
 		expect(hero.power_up).to eq 110
 	end
+
 	it "can power down" do
 		hero = Hero.new 'yan'
 
 		expect(hero.power_down).to eq 90
+	end
+
+	it "displays full hero_info" do
+		hero = Hero.new 'yan'
+
+		expect(hero.hero_info).to eq "Yan has a health of 100"
 	end
 end
